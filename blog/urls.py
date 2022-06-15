136f67980f1519d1591609d69000498973fc1e13
hiep <lionelhiepga@gmail.com>
2022-06-15 10:13:11 +0700

    CO - du an thu n

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,5 @@ from . import views
 # app_name = "hold"
 urlpatterns = [
     path('', views.index, name = 'blog'),
-    path('<int:id>', views.PostDetail, name = 'detail'),
+    path('<int:id>', views.PostDetail, name = 'detailPost'),
 ]
--- luachon/urls.py
+++ luachon/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 from . import views
 
-# app_name = "hold"
+app_name = "luachon"
 urlpatterns = [
     path('', views.index, name = 'index'),
-    path("detail/<int:question_id>", views.detail, name = 'detail'),
+    path('detail/<int:question_id>', views.detail, name = "detail"),
     path('list/', views.xemds_question, name = 'list'),
     path('<int:question_id>', views.vote, name= "vote"),
 ]
